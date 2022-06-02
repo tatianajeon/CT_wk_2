ac622d8a387aed49634310f1d3211e1ee367e72b
tatiana <105023527+tatianajeon@users.noreply.github.com>
2022-06-01 21:08:07 -0500

    day_3 + hw added

---
+++ day_3/circle.py
@@ -0,0 +1,7 @@
+from cmath import pi
+
+
+import math 
+
+def circle(radius):
+    return 2 * pi * radius
---
+++ day_3/sqft_module.py
@@ -0,0 +1,3 @@
+def square_footage(length, width):
+    return (f"Your homesquare footage is {length * width}")
+
